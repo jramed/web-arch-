613c89a5004e471afb2baaeb515517cdff24cc84
Juan Ramon Medina <jrame@gmail.com>
2018-10-28 20:34:26 +0100

    Add controller for main page and DB storing
    The data are stored in a H2 DB.
    The .hthml template file updated to mustuche
    to show the message when there is not any customer order
    and to show the id of the order when there is some order
    place.
    This is still in a very initial state

--- src/main/java/webarch/essd/upm/es/MainPageController.java
+++ src/main/java/webarch/essd/upm/es/MainPageController.java
@@ -1,15 +1,66 @@
 package webarch.essd.upm.es;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import webarch.essd.upm.es.model.CustomerOrder;
+import webarch.essd.upm.es.model.Product;
+
 @Controller
 public class MainPageController {
 	
-	@GetMapping("/")
-	public String mainPage(Model model) {
-		return "mainPage";
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private CustomerOrderRepository customerOrderRepository;
+	
+	@PostConstruct
+	public void init() {
+
+		CustomerOrder order1 = new CustomerOrder("Selección");
+		CustomerOrder order2 = new CustomerOrder("FC Barcelona");
+		CustomerOrder order3 = new CustomerOrder("Atlético de Madrid");
+		
+		customerOrderRepository.save(order1);
+		customerOrderRepository.save(order2);
+		customerOrderRepository.save(order3);
+		
+		Product p1 = new Product("product1");
+		Product p2 = new Product("product2");
+		
+		p1.getOrders().add(order1);
+		p1.getOrders().add(order2);
+		
+		p2.getOrders().add(order1);
+		p2.getOrders().add(order3);
+		
+		productRepository.save(p1);
+		productRepository.save(p2);
+}
+	
+	@GetMapping("/wa-main-page")
+	public String customerOrders(Model model) {
+		List<CustomerOrder> customerOrders = customerOrderRepository.findAll();
+		boolean showEmptyOrdersMsg = false ;
+		boolean showOrders = false;
+		if(0 == customerOrders.size()) {
+			showEmptyOrdersMsg = true;
+		}
+		else {
+			showOrders = true;
+		}
+		System.out.println("The DB content is: " + customerOrders);
+		model.addAttribute("showEmptyOrdersMsg", showEmptyOrdersMsg);
+		model.addAttribute("showOrders", showOrders);
+		model.addAttribute("customerOrders", customerOrders);
+		return "wa-main-page";
 	}
 
 }
