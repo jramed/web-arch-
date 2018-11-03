07c90ccbe939488133f8a246c34d4a0efde159fe
Juan Ramon Medina <jrame@gmail.com>
2018-11-03 19:27:08 +0100

    Remove traces
    This version does not attend the change event
    use $('.form-form input').on('change keyup',function() {
    to control an item writen or selected from the list

--- src/main/resources/static/js/disable_order_submit_button.js
+++ src/main/resources/static/js/disable_order_submit_button.js
@@ -1,27 +1,21 @@
 $(document).ready(function(){
 	$('#submit-order-button').attr('disabled',true);
-	console.log("before funcion");
-//	$('.form-control').keyup(function(){
-	$('input').keyup(function(){
+
+	$('.form-form input').keyup(function(){
 
         var empty = false;
-        $('input').each(function() {
-            if ($(this).val().length == 0) {
+        $('.form-form input').each(function() {
+            if ($.trim($(this).val()).length == 0) {
                 empty = true;
-                console.log("set empty a true for: " + this.value);
             }
         });
 
-//		if($.trim($(this).val()).length !=0){
         if (!empty) {
 			$('#submit-order-button').attr('disabled', false);
-			console.log("enabled");
 		}
 		else
 		{
 			$('#submit-order-button').attr('disabled', true);
-			console.log("disabled");
 		}
 	})
-	console.log("function end");
 });
