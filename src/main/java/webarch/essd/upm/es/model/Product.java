798c5e1427569d996d5c22286b6e8fd5a74ace21
Juan Ramon Medina <jrame@gmail.com>
2018-11-11 15:49:36 +0000

    Remove not used method equalsStrict

--- src/main/java/webarch/essd/upm/es/model/Product.java
+++ src/main/java/webarch/essd/upm/es/model/Product.java
@@ -88,24 +88,6 @@ public class Product {
 		return false;
 	}
 	
-	public boolean equalsStrict(Object obj) {
-		if (obj == null) {
-			return false;
-		}
-		
-		if (!Product.class.isAssignableFrom(obj.getClass())) {
-			return false;
-		}
-			
-		final Product other = (Product)obj;
-		if (this.getName().equals(other.getName()) &&
-				this.getStatus().equals(other.getStatus())) {
-			return true;
-		}
-		
-		return false;
-	}
-	
 	public boolean equalsStatus(Object obj) {
 		if (obj == null) {
 			return false;
